46c7f20a74a17ab8523d0c4477ee6d5088ace03e
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2017-11-28 01:56:25 +0600

    Implement bubble sort

---
+++ BubbleSorter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    public class BubbleSorter
+    {
+        public static void DoSort(int[] list)
+        {
+            list = Sort(list);
+        }
+        private static int[] Sort(int[] list)
+        {
+            
+            for(var n = list.Length; n != 0;)
+            {
+                var newN = 0;
+                for (int i = 1; i <= (n-1); i++)
+                {
+                    if(list[i-1] > list[i])
+                    {
+                        int temp = list[i - 1];
+                        list[i - 1] = list[i];
+                        list[i] = temp;
+                        newN = i;
+                    }
+                }
+                n = newN;
+            }
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 42, 1, 11, 30 };
+            BubbleSorter.DoSort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+                
+            }
+            Console.Read();
+        }
+    }
+}
